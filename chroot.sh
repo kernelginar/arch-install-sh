f88d317da15c000c6ad3c4fe9031cd802103af89
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-08-30 14:54:17 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -20,11 +20,15 @@ if [ $localeset == "en" ];
 then
     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
     locale-gen
+    touch /etc/locale.conf
+    echo "LANG=en_US.UTF-8"
     touch /etc/vconsole.conf
     echo "KEYMAP=us" >> /etc/vconsole.conf
 else
     echo "tr_TR.UTF-8 UTF-8" >> /etc/locale.gen
     locale-gen
+    touch /etc/locale.conf
+    echo "LANG=tr_TR.UTF-8"
     touch /etc/vconsole.conf
     echo "KEYMAP=trq" >> /etc/vconsole.conf
 fi
